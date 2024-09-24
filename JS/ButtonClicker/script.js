3a1c84bb2309bcd42368f7093be8b63f01928cca
KiyokoNee <k.c.gearing@gmail.com>
2024-09-23 19:56:38 -0500

    did what I could, we'll go over it if you're willing

--- JS/ButtonClicker/script.js
+++ JS/ButtonClicker/script.js
@@ -1,3 +1,4 @@
+/* switches the text of the login button. uses querySelector as the login button is the first one on the page */
 function login() {
     const loginText = document.querySelector('button');
     if(loginText.innerText === "Login") {
